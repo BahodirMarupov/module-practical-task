f9c419c7b3d3ba4e0fac125ebe243cd5c92322ba
Bakhodir Marupov <bakhodirjon_marupov@epam.com>
2022-06-14 12:18:05 +0500

    16. Add module-info.java

---
+++ application/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Author: Bakhodirjon_Marupov
+ * Date: 14/06/2022
+ */
+module application {
+    requires jmp.cloud.bank.impl;
+    requires jmp.cloud.service.impl;
+    requires jmp.dto;
+}
