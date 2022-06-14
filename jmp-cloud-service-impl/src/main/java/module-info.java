4e3479b96d75ba2a703a4b3ba61f4448bfa48458
Bakhodir Marupov <bakhodirjon_marupov@epam.com>
2022-06-14 12:04:54 +0500

    12. Add module-info.java

---
+++ jmp-cloud-service-impl/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Author: Bakhodirjon_Marupov
+ * Date: 14/06/2022
+ */
+module jmp.cloud.service.impl {
+    requires jmp.dto;
+    requires transitive jmp.service.api;
+    exports jgm.workshop.cloud.service.impl;
+}
